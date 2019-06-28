3454d1e4d8dd1dcf848fa5a4fe93d22fbe581870
Dumas Carvalho <dumasmcarvalho@gmail.com>
2019-06-28 00:43:14 -0300

    Update CalculadoraRunner.java

--- src/test/java/ifsp/bdd/calculadora/CalculadoraRunner.java
+++ src/test/java/ifsp/bdd/calculadora/CalculadoraRunner.java
@@ -7,8 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features", tags = "@CalculadoraTeste", 
-glue = "ifsp.bdd.calculadora" , monochrome = true, dryRun = false,
-plugin = {"pretty", "html:target/cucumber-html-report"})
+                 glue = "ifsp.bdd.calculadora", monochrome = true, dryRun = false,
+                 plugin = {"pretty", "html:target/cucumber-html-report"})
 
 public class CalculadoraRunner {
 
